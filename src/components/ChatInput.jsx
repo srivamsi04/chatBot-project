9a987d73d786f943b452d01a9cef6f058415380c
srivamsio4 <pentakotasri204@gmail.com>
2025-07-24 17:41:43 +0530

    tailwind hover effect

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -29,9 +29,12 @@ export function ChatInput({chatMessages,setChatMessages}){
     setInputText('');
     }
     return(
-        <div className="chat-input-container">
+        <div /*className="chat-input-container"*/
+            className="flex mb-[10px]"
+        >
         <input 
-            className="chat-input" 
+            //className="chat-input" 
+            className="flex-grow-1 mr-[10px] py-[12px] px-[15px] rounded-lg border-1 border-[#cccccc] "
             placeholder="Send a message to Chatbot" 
             size="30" 
             onChange={saveInputText}
@@ -40,7 +43,7 @@ export function ChatInput({chatMessages,setChatMessages}){
         <button
             onClick={sendMessages}
             //className="send-Button"
-            className="bg-[rgb(25,135,84)] py-[10px] px-[20px] rounded-lg text-white"
+            className=" bg-[rgb(25,135,84)] py-[10px] px-[20px] rounded-lg text-white hover:bg-[rgba(208, 125, 10, 1)]"
         >
             Send
         </button>
